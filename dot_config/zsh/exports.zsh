7cac8dc2d8abbebdce5545e5f5bed0ee3630c23a
Vincent Lossel <hello@vincentlossel.com>
2022-11-20 21:58:09 +0100

    updated ssh agent

--- dot_config/zsh/exports.zsh
+++ dot_config/zsh/exports.zsh
@@ -3,6 +3,9 @@
 # ZSH
 export ZSH="${HOME}/.oh-my-zsh"
 
+# SSH
+export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+
 # Editor
 export EDITOR=nvim
 export VISUAL=$EDITOR
