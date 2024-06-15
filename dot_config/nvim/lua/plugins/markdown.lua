edbe09d62beefdd1fe2e36981e495e3674eadf50
Vincent Lossel <hello@vincentlossel.com>
2024-06-15 09:38:41 +0200

    update markdown setup

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -1,6 +1,36 @@
 return {
-	-- Markdown Preview
+	-- Headlines
 	{
-		"iamcco/markdown-preview.nvim",
+		"lukas-reineke/headlines.nvim",
+		after = "nvim-treesitter",
+		config = function()
+			require("headlines").setup()
+		end,
+	},
+	-- Live Preview
+	{
+		"ellisonleao/glow.nvim",
+		config = function()
+			require("glow").setup()
+		end,
+	},
+	-- Obsidian-like Features
+	{
+		"epwalsh/obsidian.nvim",
+		config = function()
+			require("obsidian").setup({
+				workspaces = {
+					{
+						name = "Personal",
+						path = "~/Documents/Notes/Personal",
+					},
+					{
+						name = "Work",
+						path = "~/Documents/Notes/Work",
+					},
+				},
+				-- TODO: Configure templates, picker (telescope), and journal
+			})
+		end,
 	},
 }
