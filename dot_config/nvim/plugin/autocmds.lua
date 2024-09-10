dd544a6d331f7db9e01e1e1b8c0fc063ae31c278
Vincent Lossel <hello@vincentlossel.com>
2024-09-10 08:53:52 +0200

    enhance(nvim): Add LSP autocmds

--- dot_config/nvim/plugin/autocmds.lua
+++ dot_config/nvim/plugin/autocmds.lua
@@ -2,9 +2,22 @@
 
 -- Highlight when yanking text
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	desc = "Highlight when yanking text",
+	group = vim.api.nvim_create_augroup("HighlightYanking", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
+	end,
+})
+
+-- LSP Autocmds
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "Enable LSP inlay hints if available",
+	group = vim.api.nvim_create_augroup("UserLSPConfig", {}),
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client.server_capabilities.inlayHintProvider then
+			-- Enable inlay hints if available
+			vim.lsp.inlay_hint.enable(true, args.buf)
+		end
 	end,
 })
