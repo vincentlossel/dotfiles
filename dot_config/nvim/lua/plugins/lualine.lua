61f3f839f5628770ba366f1ec5c0eacff0953b52
Vincent Lossel <hello@vincentlossel.com>
2024-09-10 08:45:22 +0200

    enhance(nvim): Update lualine setup

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -39,7 +39,7 @@ return {
 						{ "filename", path = 1 },
 					},
 					lualine_x = { "filetype" },
-					lualine_y = {},
+					lualine_y = { "progress" },
 					lualine_z = { "location" },
 				},
 			}
